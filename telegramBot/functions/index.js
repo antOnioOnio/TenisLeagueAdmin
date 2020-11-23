b7af5e59d33c0fbe2d3473bf7578aa1590458f8d
Antonio Garcia <anthoniusstanfield@gmail.com>
2020-11-23 14:11:51 +0100

    bot fixed

--- telegramBot/functions/index.js
+++ telegramBot/functions/index.js
@@ -2,24 +2,19 @@ const functions = require('firebase-functions');
 const { Telegraf } = require('telegraf');
 
 
-let config = require('../env.json');
+let config = require('./env.json');
 
 if (Object.keys(functions.config()).length){
   config = functions.config();
 }
 
-const bot = new Telegraf(config.service.telegram_key);
+console.log(config.service.telegram_key);
 
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+const bot = new Telegraf(config.service.telegram_key);
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 bot.launch();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.bot = functions.https.onRequest((req, res) => {
+  bot.handleUpdate(req.body, res);
+})
+
