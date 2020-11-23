92b207c249127dbfb22033beb2d8e769bd2e3d97
Antonio Garcia <anthoniusstanfield@gmail.com>
2020-11-23 12:43:42 +0100

    bot structure done #44 #38

--- telegramBot/functions/index.js
+++ telegramBot/functions/index.js
@@ -1,4 +1,20 @@
 const functions = require('firebase-functions');
+const { Telegraf } = require('telegraf');
+
+
+let config = require('../env.json');
+
+if (Object.keys(functions.config()).length){
+  config = functions.config();
+}
+
+const bot = new Telegraf(config.service.telegram_key);
+
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
