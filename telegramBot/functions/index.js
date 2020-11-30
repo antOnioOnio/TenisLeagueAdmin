7f6ddec69d1b0a7bc3035bf4e387de15ec284db3
Antonio Garcia <anthoniusstanfield@gmail.com>
2020-11-30 22:15:34 +0100

    bot working with fecth [skip ci]

--- telegramBot/functions/index.js
+++ telegramBot/functions/index.js
@@ -1,9 +1,8 @@
 const functions = require('firebase-functions');
-const axios = require('axios')
 const { Telegraf } = require('telegraf');
-
-
+const fetch = require('node-fetch');
 let config = require('./env.json');
+
 
 if (Object.keys(functions.config()).length){
   config = functions.config();
@@ -11,15 +10,22 @@ if (Object.keys(functions.config()).length){
 
 const bot = new Telegraf(config.service.telegram_key);
 
+var urlPlayers = "https://tenis-league-admin.vercel.app/api/players";
+var urlMatches = "https://tenis-league-admin.vercel.app/api/matches";
 
 
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+var settings = { method: "Get" };
+
+var options = "/All \t Muestra todos los partidos de la actual liga.\n" ;
+    options+= "/Today \tMuestra todos los partidos que se juegan hoy \n";
+    options+=  "/Players \t Muestra los jugadores de la actual liga ";
+
+
+bot.hears('hi', (ctx) => ctx.reply('Hola ! como puedo ayudarte ? las opciones son :\n' + options));
 
 bot.launch();
 
-
-// Posibilites
-bot.help(ctx => ctx.reply('/All \t Muestra todos los partidos de la actual liga.\n/Today \tMuestra todos los partidos que se juegan hoy \n/Players \t Muestra los jugadores de la actual liga' ))
+bot.help(ctx => ctx.reply(options))
 
 
 /**
@@ -27,50 +33,76 @@ bot.help(ctx => ctx.reply('/All \t Muestra todos los partidos de la actual liga.
  */
 bot.command('Today', async (ctx) => {
 
-  let res = 'Los partidos que se juegan hoy son:\n'
+  let res = 'Los partidos que se juegan hoy son:\n';
+
+  await fetch(urlMatches+"?date=today", settings)
+    .then(res => res.json())
+    .then((json) => {
+
+      if ( json.length > 0){
+
+        for( var i = 0; i < json.length ; i++){ 
+          res +=  "" + json[i]._player1 + " vs " + json[i]._player2 + "\n";
+        }
+
+    }
+
+    return null;
+  }) 
+
   
-  const response = await axios.get(config.service.api_today)
-
-  if ( response.data.length > 0){
-      for( var i = 0; i < response.data.length ; i++){ 
-        res +=  "" +response.data[i]._player1 + " vs " + response.data[i]._player2 + "\n";
-      }
-
-  }
   ctx.reply(res)
 })
+
+async function getMatchesToday(){
+  
+}
 
 /**
  * 
  */
 bot.command('All', async (ctx) => {
 
-  let res = 'Todos los partidos de la liga son:\n'
-  
-  const response = await axios.get(config.service.api_matches)
+  let res = 'Todos los partidos de la liga son:\n';
+  await fetch(urlMatches, settings)
+    .then(res => res.json())
+    .then((json) => {
 
-  if ( response.data.length > 0){
-      for( var i = 0; i < response.data.length ; i++){ 
-        res +=  "" +response.data[i]._player1 + " vs " + response.data[i]._player2 + "\n";
+      if ( json.length > 0){
+        for( var i = 0; i < json.length ; i++){ 
+          res +=  "" +json[i]._player1 + " vs " + json[i]._player2 + "\n";
+        }
       }
-  }
+
+      return null;
+  })  
+
+  
   ctx.reply(res)
 })
+
+
 
 /**
  * 
  */
 bot.command('Players', async (ctx) => {
 
-  let res = 'Todos los jugadores de la liga son:\n'
-  
-  const response = await axios.get(config.service.api_players)
+  let res = 'Todos los jugadores de la liga son:\n';
 
-  if ( response.data.length > 0){
-      for( var i = 0; i < response.data.length ; i++){ 
-        res +=  "" +response.data[i]._name + "\n";
+  await fetch(urlPlayers, settings)
+  .then(res => res.json())
+  .then((json) => {
+
+    if ( json.length > 0){
+      for( var i = 0; i < json.length ; i++){ 
+        res +=  "" +json[i]._name +"\n";
       }
-  }
+    }
+    return null;
+  })  
+
+
   ctx.reply(res)
 })
 
