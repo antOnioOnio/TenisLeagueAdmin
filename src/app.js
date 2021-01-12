c1efc5f1be9a1102ede00ecad3b2f31ad6226dde
Antonio Garcia <anthoniusstanfield@gmail.com>
2021-01-12 22:04:59 +0100

    procfile added

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const Hapi = require('@hapi/hapi');
 const routes = require("./routes");
 
 const server = Hapi.server({
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 5000,
     host: 'localhost',
     app: {}
 });
